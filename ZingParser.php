63564bf31965d51478075dd5ee26dff895a8dea8
Tung Nguyen Son <tung.nguyen@tiki.vn>
2015-12-15 22:03:23 +0700

    reformat

--- ZingParser.php
+++ ZingParser.php
@@ -1,59 +1,62 @@
 <?php
 
-class ZingParser {
+class ZingParser
+{
 
-        public function match($text) {
-                $result = strpos($text, 'http://mp3.zing.vn/bai-hat/');
+    public function match($text)
+    {
+        $result = strpos($text, 'http://mp3.zing.vn/bai-hat/');
 
-                return $result !== FALSE;
+        return $result !== FALSE;
+    }
+
+    public function getMedia($url)
+    {
+
+        $url = preg_replace('/.*(http:\/\/mp3\.zing\.vn\/bai\-hat\/.*\.html).*/i', "$1", $url);
+        $page = $this->getGzipLink($url);
+
+        $pattern = 'xmlURL=(http:\/\/mp3\.zing\.vn\/xml\/song\-xml\/[a-zA-Z0-9]+)';
+        $matches = [];
+        $result = preg_match("/$pattern/", $page, $matches);
+
+        //var_dump($matches);
+
+        if ($result) {
+
+            $page = $this->getGzipLink($matches[1]);
+
+            $pattern = '\<source\>\<\!\[CDATA\[(.*)\]\]\>\<\/source\>';
+            preg_match("/$pattern/", $page, $matches);
+            $url = $matches[1];
+
+            $pattern = '\<title\>\<\!\[CDATA\[(.*)\]\]\>\<\/title\>';
+            preg_match("/$pattern/", $page, $matches);
+            $title = $matches[1];
+
+            return [
+                'title' => $title,
+                'url' => $url
+            ];
+        } else {
+            return FALSE;
         }
 
-        public function getMedia($url) {
+    }
 
-		$url = preg_replace('/.*(http:\/\/mp3\.zing\.vn\/bai\-hat\/.*\.html).*/i', "$1", $url);
-                $page = $this->getGzipLink($url);
+    public function getGzipLink($url)
+    {
 
-                $pattern = 'xmlURL=(http:\/\/mp3\.zing\.vn\/xml\/song\-xml\/[a-zA-Z0-9]+)';
-                $matches = [];
-                $result = preg_match("/$pattern/", $page, $matches);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_ENCODING, "gzip");
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 GTB5');
+        $output = curl_exec($ch);
+        curl_close($ch);
 
-                //var_dump($matches);
+        return $output;
 
-		if ($result) {
-
-                $page = $this->getGzipLink($matches[1]);
-
-                $pattern = '\<source\>\<\!\[CDATA\[(.*)\]\]\>\<\/source\>';
-                preg_match("/$pattern/", $page, $matches);
-                $url = return $matches[1];
-
-                $pattern = '\<title\>\<\!\[CDATA\[(.*)\]\]\>\<\/title\>';
-                preg_match("/$pattern/", $page, $matches);
-                $title = return $matches[1];
-
-		return [
-			'title' => $title,
-			'url' => $url
-		];
-		}
-		else {
-			return FALSE;
-		}
-
-        }
-
-        public function getGzipLink($url) {
-
-          $ch = curl_init();
-          curl_setopt($ch, CURLOPT_URL, $url);
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-          curl_setopt($ch,CURLOPT_ENCODING , "gzip");
-          curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 GTB5');
-          $output = curl_exec($ch);
-          curl_close($ch);
-
-          return $output;
-
-        }
+    }
 
 }
--- index.php
+++ index.php
@@ -4,49 +4,43 @@ require_once 'MopidyRemote.php';
 
 define('BOT_PREFIX', 'slackpibot: ');
 
-function echoSlackText($text) {
-        $text = $text;
-        echo json_encode(['text' => $text]);
+function echoSlackText($text)
+{
+    $text = $text;
+    echo json_encode(['text' => $text]);
 }
 
-        $params = $_REQUEST;
-        $text = isset($params['text'])?$params['text']:'';
+$params = $_REQUEST;
+$text = isset($params['text']) ? $params['text'] : '';
 
-	$remote = new MopidyRemote();
+$remote = new MopidyRemote();
 
-        $parser = new ZingParser();
+$parser = new ZingParser();
 
-        if (strpos($text, BOT_PREFIX) === 0) {
-                // Ignore
-        }
-        else if ($text == 'next') {
-		$remote->next();
-                echoSlackText('Neeeeeeeeeext!');
-        }
-        else if ($text == 'play') {
-                $remote->play();
-                echoSlackText('Ok!');
-        }
-        else if ($text == 'stop') {
-                $remote->stop();
-                echoSlackText('Shhh!');
-        }
-        else if ($text == 'clear') {
-                $remote->clear();
-                echoSlackText('Empty!');
-        }
-        else if ($text !== '') {
-                if ($parser->match($text)) {
-			$mediaData = $parser->getMedia($text);
-			$remote->add($mediaData);
-                        echoSlackText("Received");
-                }
-                else {
+if (strpos($text, BOT_PREFIX) === 0) {
+    // Ignore
+} else if ($text == 'next') {
+    $remote->next();
+    echoSlackText('Neeeeeeeeeext!');
+} else if ($text == 'play') {
+    $remote->play();
+    echoSlackText('Ok!');
+} else if ($text == 'stop') {
+    $remote->stop();
+    echoSlackText('Shhh!');
+} else if ($text == 'clear') {
+    $remote->clear();
+    echoSlackText('Empty!');
+} else if ($text !== '') {
+    if ($parser->match($text)) {
+        $mediaData = $parser->getMedia($text);
+        $remote->add($mediaData);
+        echoSlackText("Received");
+    } else {
         //                echoSlackText('Not Match');
-                }
-        }
-	else {
-		echo BOT_PREFIX.'Invalid parameters';
-	}
+    }
+} else {
+    echo BOT_PREFIX . 'Invalid parameters';
+}
 
 
