c2b597b63d2fe7a0a5917e14bd3c68ef237ed265
root <root@raspberrypi>
2015-12-15 21:56:23 +0700

    init

---
+++ MopidyRemote.php
@@ -0,0 +1,96 @@
+<?php
+
+class MopidyRemote {
+
+  protected $_curl;
+
+  public function __construct() {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://125.234.98.126:6680/mopidy/rpc');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");      
+ 
+    $this->_curl = $ch;
+
+  }
+
+  public function __destruct() {
+    curl_close($this->_curl);
+  }
+
+/*
+{
+  "method": "core.playback.stop",
+  "jsonrpc": "2.0",
+  "params": {},
+  "id": 1
+}
+*/
+  protected function _createParam() {
+    $obj = [
+      'jsonrpc' => "2.0",
+      'params' => [],
+      'id' => 1
+    ];
+    return $obj;
+  }
+
+  protected function _exec($params) {
+    $paramsStr = json_encode($params);
+
+    curl_setopt($this->_curl, CURLOPT_POSTFIELDS, $paramsStr); 
+        curl_setopt($this->_curl, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($paramsStr))                                                                       );
+
+    $result = curl_exec($this->_curl);
+file_put_contents('/tmp/slackpi', $result);
+    return $result;
+  }
+
+  public function stop() {
+    $params = $this->_createParam();
+    $params['method'] = 'core.playback.stop';
+    $this->_exec($params);
+    
+  }
+
+  public function add($data) {
+
+    $params = $this->_createParam();
+    $params['method'] = 'core.tracklist.add';
+    $params['params'] = [
+      'uri' => $data['url]
+    ];
+    $result = $this->_exec($params);
+//    $data = json_decode($result, 1);
+
+//    $trackId = $data['result'][0]['tlid'];
+
+//   $this->play($trackId);
+
+  }
+
+  public function next() {
+    $params = $this->_createParam();
+    $params['method'] = 'core.playback.next';
+    $this->_exec($params);
+  }
+
+  public function play() {
+    $params = $this->_createParam();
+    $params['method'] = 'core.playback.play';
+    $params['params'] = [
+    ];
+    $this->_exec($params);
+
+  }
+
+  public function clear() {
+    $params = $this->_createParam();
+    $params['method'] = 'core.tracklist.clear';
+    $this->_exec($params);
+  }
+
+}
+
---
+++ testMopidyRemote.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'MopidyRemote.php';
+
+$o = new MopidyRemote();
+$o->play(15);
