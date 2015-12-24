d3462c5f4c58cedcf770f7c90fd30eb452665660
Tung Nguyen Son <tung.nguyen@tiki.vn>
2015-12-25 01:09:49 +0700

    refactor local config

--- config/main.php
+++ config/main.php
@@ -1,18 +1,13 @@
 <?php
 
 return [
-    'mopidy' => [
-        'url' => 'http://125.234.98.126:6680/mopidy/rpc',
-    ],
     'parser' => [
         'plugins' => ['Zingmp3', 'Nhaccuatui'],
     ],
+    'mopidy' => [
+    ],
     'queue' => [
-
     ],
     'slack' => [
-        'webhook_url' => 'https://hooks.slack.com/services/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',
     ],
-    'log_path' => '/tmp/pimusic/log',
-    'cache_path' => '/tmp/pimusic/cache',
 ];
