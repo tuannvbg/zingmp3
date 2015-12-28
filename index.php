a202484d0470718c807c75099c626430bdab385c
Tung Nguyen Son <tung.nguyen@tiki.vn>
2015-12-29 02:55:12 +0700

    Reinvent

--- index.php
+++ index.php
@@ -19,23 +19,23 @@ if ($text != '' && strpos($text, BOT_PREFIX) !== 0) {
         case 'help':
         case '?':
             $responseText = "" .
-                "- Info: `?`, `intro`, `status`, `wake up`, `sleep`\n".
+                "- Info: `?`, `intro`, `status`, `wake up`, `sleep`\n" .
                 "- Tracklist: `current`, `list`\n" .
                 "- Playback: `next`, `play`, `stop`, `pause`, `resume`\n" .
-                "- Supported links từ *ZingMP3*, *Nhaccuatui*. Site khác không quan tâm\n" . 
-		"    Format: song, album, playlist (public hoặc cá nhân) \n".
-		"    Ví dụ: \n".
-		"    http://mp3.zing.vn/bai-hat/Minh-Yeu-Tu-Bao-Gio-Em-La-Ba-Noi-Cua-Anh-OST-Miu-Le/ZW7WFEIW.html";
+                "- Supported links từ *ZingMP3*, *Nhaccuatui*. Site khác không quan tâm\n" .
+                "    Format: song, album, playlist (public hoặc cá nhân) \n" .
+                "    Ví dụ: \n" .
+                "    http://mp3.zing.vn/bai-hat/Minh-Yeu-Tu-Bao-Gio-Em-La-Ba-Noi-Cua-Anh-OST-Miu-Le/ZW7WFEIW.html";
             break;
         case 'intro':
             $responseText = "" .
                 "*SlackPi* là tool giúp nghe nhạc theo yêu cầu\n" .
-		"và được tạo bởi 1 tên lười lết mông tới chỗ Ampli để chuyển bài hát \n" .
+                "và được tạo bởi 1 tên lười lết mông tới chỗ Ampli để chuyển bài hát \n" .
                 "Đối tượng sử dụng: Thích nghe nhạc và độ lười trên trung bình \n\n" .
-		"Tự mở từ 8h30-9h30 a.m, 4h30-10h p.m hàng ngày trừ T7,CN\n\n".
+                "Tự mở từ 8h30-9h30 a.m, 4h30-10h p.m hàng ngày trừ T7,CN\n\n" .
                 "500đ document: <http://docs.tiki.com.vn/display/TECH/PiMusic> \n" .
-                "Code: <https://github.com/tungbi/slackpi> \n".
-		"Anh em sửa, Pull Request thoải mái";
+                "Code: <https://github.com/tungbi/slackpi> \n" .
+                "Anh em sửa, Pull Request thoải mái";
             break;
         case 'next':
             $remote->next();
@@ -72,7 +72,7 @@ if ($text != '' && strpos($text, BOT_PREFIX) !== 0) {
         case 'hey':
         case 'status':
             $isRunning = $remote->getServiceStatus();
-            $responseText = ($isRunning)?"Vẫn sống nhăn :grin:":":zzz:";
+            $responseText = ($isRunning) ? "Vẫn sống nhăn :grin:" : ":zzz:";
             break;
         case 'wakeup':
         case 'wake up':
@@ -84,13 +84,13 @@ if ($text != '' && strpos($text, BOT_PREFIX) !== 0) {
             else {
                 $remote->startService();
                 $isRunning = $remote->getServiceStatus();
-                $responseText = ($isRunning)?"Hế nhô :kissing_smiling_eyes:":":zzz:";
+                $responseText = ($isRunning) ? "Hế nhô :kissing_smiling_eyes:" : ":zzz:";
             }
             break;
         case 'sleep':
             $remote->stopService();
             $isRunning = $remote->getServiceStatus();
-            $responseText = ($isRunning)?"Ko hiểu sao nhưng vưỡn tỉnh như sáo :flushed:":":zzz:";
+            $responseText = ($isRunning) ? "Ko hiểu sao nhưng vưỡn tỉnh như sáo :flushed:" : ":zzz:";
             break;
         default:
             $result = \App::getParser()->match($text);
