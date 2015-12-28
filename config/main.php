5bd1e3c1cebc5be59a900c4879843543a3b9fcd8
Tung Nguyen Son <tung.nguyen@tiki.vn>
2015-12-28 19:52:30 +0700

    re-add main config

---
+++ config/main.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'app_name' => 'PiMusic',
+    'parser' => [
+        'plugins' => ['Zingmp3', 'Nhaccuatui'],
+    ],
+    'mopidy' => [
+    ],
+    'queue' => [
+    ],
+    'slack' => [
+    ],
+];
